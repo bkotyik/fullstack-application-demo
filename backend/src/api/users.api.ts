bec18ac06d48b03c0383b5b02517c6d1678b46e7
bkotyik <bkotyik@sztaki.hu>
2016-10-13 23:40:24 +0200

    backend: Added users.api route configuration. configured /users endpoint to use users.api

---
+++ backend/src/api/users.api.ts
@@ -0,0 +1,9 @@
+import {Router} from "express";
+
+export default function UsersApi(deps?: any) {
+
+    var router = Router();
+    // TODO: Add endpoint declarations
+
+    return router;
+}
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 /// <reference path="../tsd.d.ts" />
 
 import * as express from "express";
+import UsersApi from "./api/users.api";
 
 var app = express();
+
+app.use("/users", UsersApi());
 
 app.listen(3000, function () {
     console.log("Application app listening on port 3000!");
