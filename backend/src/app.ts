fa9a8c27a1b7be6f4aff61a156d08807780b532e
bkotyik <bkotyik@sztaki.hu>
2016-10-14 23:30:48 +0200

    backend: installed and configured json body parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@
 
 import * as express from "express";
 import UsersApi from "./api/users.api";
+import * as bodyParser from "body-parser";
 
 var app = express();
 
+app.use(bodyParser.json());
 app.use("/users", UsersApi());
 
 app.listen(3000, function () {
