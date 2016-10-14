38d860adbddd922f9841ba6244672aa58b297146
bkotyik <bkotyik@sztaki.hu>
2016-10-14 23:19:43 +0200

    backend: refactored AddUserMiddleware

--- backend/src/middlewares/AddUserMiddleware.ts
+++ backend/src/middlewares/AddUserMiddleware.ts
@@ -1,18 +1,14 @@
 /// <reference path="../../tsd.d.ts" />
 import {Request, Response} from "express";
-import UserModel from "../models/user.model";
+import User from "../models/user.model";
 
 export default function AddUserMiddleware(deps?: any) {
-    return function(request: Request, response: Response, next: Function) {
-        let model: UserModel = null;
-        try {
-            model = new UserModel(request.body);
-        } catch (error) {
-            next(error);
-        }
+    return function (request: Request, response: Response, next: Function) {
+        let model: User = new User(request.body);
 
-        // TODO: Check if model is valid
-
-        response.json({message: "Thank you for your application. We will contact you soon."});
+        model.validate().then(
+            (user: User) => response.json({message: "Thank you for your application. We will contact you soon."}),
+            (error: any) => next(error)
+        );
     };
 }
