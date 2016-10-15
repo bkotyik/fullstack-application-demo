ee4d20cb324facff22dee2d76e037ec60374cf29
bkotyik <bkotyik@gmail.com>
2016-10-15 22:19:08 +0200

    Revert "frontend: added ng2-bootstrap and bootstrap stylesheet"
    This reverts commit 0dd4c8a1c2fb3e27c217bb65d8a46e228c40aeaf.

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -4,30 +4,7 @@ import {Component} from '@angular/core';
     selector: 'my-home',
     templateUrl: './home.component.html'
 })
-export class HomeComponent {
-
-    public oneAtATime: boolean = true;
-    public items: Array<string> = ['Item 1', 'Item 2', 'Item 3'];
-
-    public status: Object = {
-        isFirstOpen: true,
-        isFirstDisabled: false
-    };
-
-    public groups: Array<any> = [
-        {
-            title: 'Dynamic Group Header - 1',
-            content: 'Dynamic Group Body - 1'
-        },
-        {
-            title: 'Dynamic Group Header - 2',
-            content: 'Dynamic Group Body - 2'
-        }
-    ];
-
-    public addItem(): void {
-        this.items.push(`Items ${this.items.length + 1}`);
-    }
+export class HomeComponent  {
 
 
 }
