0dd4c8a1c2fb3e27c217bb65d8a46e228c40aeaf
bkotyik <bkotyik@gmail.com>
2016-10-15 20:55:41 +0200

    frontend: added ng2-bootstrap and bootstrap stylesheet

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -5,14 +5,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() {
-    // Do stuff
-  }
+    public oneAtATime:boolean = true;
+    public items:Array<string> = ['Item 1', 'Item 2', 'Item 3'];
 
-  ngOnInit() {
-    console.log('Hello Home');
-  }
+    public status:Object = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
+
+    public groups:Array<any> = [
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        }
+    ];
+
+    public addItem():void {
+        this.items.push(`Items ${this.items.length + 1}`);
+    }
+
 
 }
