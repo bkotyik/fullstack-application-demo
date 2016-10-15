ed3a8aab2dc70909b84beeec6f9b23e2e891674e
bkotyik <bkotyik@gmail.com>
2016-10-15 16:12:32 +0200

    backend: Finished GetOccupationsMiddleware.spec.ts. Removed MockRequest and MockResponse. Solved the problem without them.

--- backend/src/middlewares/occupations/GetOccupationsMiddleware.spec.ts
+++ backend/src/middlewares/occupations/GetOccupationsMiddleware.spec.ts
@@ -1,30 +1,87 @@
+/// <reference path="../../../tsd.d.ts" />
+
 import OccupationsMiddleware from "./GetOccupationsMiddleware";
 import OccupationsDataSource from "../../data/occupations.datasource";
 import * as sinon from "sinon";
 import {expect} from "chai";
-import * as express from "express";
-import MockRequest from "../../test/mocks/MockRequest";
-import MockResponse from "../../test/mocks/MockResponse";
 
 describe("GetOccupationsMiddleware", function () {
+    let occupationsMiddleware: Function;
+    let mockRequest: any = null;
+    let mockResponse: any = null;
+    let next: Sinon.SinonSpy = null;
+
+    before(function() {
+        mockRequest = {};
+        mockResponse = {};
+        next = sinon.spy();
+    });
+
+    after(function() {
+       next.reset();
+    });
+
     describe("when datasource is injected", function () {
-        let occupationsMiddleware: Function;
-        let occupationsDataSourceStub: sinon.SinonStub;
-        let mockRequest: MockRequest = null;
-        let mockResponse: MockResponse = null;
+        let occupationsDataSourceStub: OccupationsDataSource;
+        let jsonSpy: sinon.SinonSpy = null;
+        let statusStub: sinon.SinonStub = null;
 
         before(function () {
-            mockRequest = new MockRequest();
-            mockResponse = new MockResponse();
-            mockResponse.status = sinon.stub();
-            mockResponse.status.returns({json: sinon.spy()});
-            occupationsDataSourceStub = sinon.stub(new OccupationsDataSource());
-            occupationsMiddleware = OccupationsMiddleware({occupationsDataSource: occupationsDataSourceStub});
-        });
+            // ARRANGE: Set up stubs, spies and mocks
+            jsonSpy = sinon.spy();
+            statusStub = sinon.stub();
 
-        it("it gets all the occupations from the data source", function () {
-            let next = sinon.spy();
+            mockResponse.status = statusStub;
+            statusStub.returns({json: jsonSpy});
+
+            occupationsDataSourceStub = new OccupationsDataSource();
+            sinon.stub(occupationsDataSourceStub);
+
+            // ACT: Inject mocked dependencies and invoke middleware
+            occupationsMiddleware = OccupationsMiddleware({occupationsDataSource: occupationsDataSourceStub});
             occupationsMiddleware(mockRequest, mockResponse, next);
         });
+
+        after(function() {
+            jsonSpy.reset();
+            statusStub.reset();
+        });
+
+        it("it does not call next middleware", function () {
+            expect(next.notCalled).be.true;
+        });
+
+        it("gets all the occupations from the data source", function() {
+            expect((<Sinon.SinonSpy>occupationsDataSourceStub.getAll).called).be.true;
+        });
+
+        it("responses in json format", function() {
+           expect(jsonSpy.called).be.true;
+        });
+
+        it("responses HTTP OK", function() {
+           expect(statusStub.withArgs(200).called).be.true;
+        });
     });
+
+    describe("when no datasource injected", function() {
+        before(function() {
+            occupationsMiddleware = OccupationsMiddleware();
+            occupationsMiddleware(mockRequest, mockResponse, next);
+        });
+
+        after(function() {
+            next.reset();
+        });
+
+        it("invokes next", function() {
+            expect(next.calledOnce).to.be.true;
+        });
+
+        it("passes an error to next function", function() {
+            expect(next.args[0].length).eq(1);
+        });
+
+    });
+
 });
