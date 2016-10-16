3bc6148c293104cee02929db40af2ff82110b30a
bkotyik <bkotyik@gmail.com>
2016-10-16 14:01:20 +0200

    backend: fix in metadata serialization. regexes will be serialized as string

--- backend/src/middlewares/users/getusermetadata.middleware.ts
+++ backend/src/middlewares/users/getusermetadata.middleware.ts
@@ -2,11 +2,20 @@
 import {Request, Response} from "express";
 import {User, WebError} from "../../models";
 
+function replacer(key: string, value: any): any {
+    if (typeof value == "object"
+        && value instanceof RegExp) {
+        return value.toString();
+    } else {
+        return value;
+    }
+}
+
 export default function AddUserMiddleware(deps?: any) {
     return function (request: Request, response: Response, next: Function) {
         if (User.Schema != null &&
             ((<any>User.Schema)._inner != null) && ((<any>User.Schema)._inner.children) != null) {
-            response.json((<any>User.Schema)._inner.children);
+            response.status(200).set("Content-Type","application/json").send(JSON.stringify((<any>User.Schema)._inner.children, replacer));
         } else {
             next(new WebError({message: "Schema data not available for User"}, 400, "NO_SCHEMA"));
         }
