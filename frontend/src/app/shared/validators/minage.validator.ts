a39d60b7309b39f4b99bf5503e1822b887fbfc7a
bkotyik <bkotyik@gmail.com>
2016-10-16 12:15:01 +0200

    frontend: fixed minage validator to handle null values

--- frontend/src/app/shared/validators/minage.validator.ts
+++ frontend/src/app/shared/validators/minage.validator.ts
@@ -9,22 +9,23 @@ export default function minAge(value: number) {
     return function (control: AbstractControl): {
         [key: string]: any;
     } {
-
         let valueInNumber = null;
-        let invalid = {
-            minAge: {
-                valid: false,
-                value: value
-            }
-        };
+        if (control.value != null) {
+            let invalid = {
+                minAge: {
+                    valid: false,
+                    value: value
+                }
+            };
 
-        try {
-            valueInNumber = Number.parseFloat(control.value);
-        } catch (error) {
-            return invalid;
+            try {
+                valueInNumber = Number.parseFloat(control.value);
+            } catch (error) {
+                return invalid;
+            }
         }
 
-        if (valueInNumber != null && valueInNumber >= value) {
+        if ( (valueInNumber != null && valueInNumber >= value) || (valueInNumber == null)) {
             return null;
         } else {
             return invalid;
