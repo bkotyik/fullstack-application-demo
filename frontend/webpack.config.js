be88200e131127fa099715ae48e57c2a9a28bc3a
bkotyik <bkotyik@gmail.com>
2016-10-17 20:44:13 +0200

    frontend: ENV 'build' -> 'production'.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -19,7 +19,7 @@ var ngtools = require('@ngtools/webpack');
 var ENV = process.env.npm_lifecycle_event;
 var isTestWatch = ENV === 'test-watch';
 var isTest = ENV === 'test' || isTestWatch;
-var isProd = ENV === 'build';
+var isProd = ENV === 'production';
 
 module.exports = function makeWebpackConfig() {
     /**
